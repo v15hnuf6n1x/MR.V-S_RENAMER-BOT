a62076a81914c95601868d44b1fb481ed98a3a15
VISHNU <Vishnuvishva2434@gmail.com>
2024-02-24 10:54:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ API_HASH = os.environ.get("API_HASH", "8cc762f4873e84a7cf0cbfd66a07244b")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "Mr_V_bots") 
 
-DB_NAME = os.environ.get("DB_NAME","Cluster0")     
+DB_NAME = os.environ.get("DB_NAME","")     
 
-DB_URL = os.environ.get("DB_URL","mongodb+srv://1234:0000@cluster0.sc33vq5.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL","")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
